891d9e71b2c51b3623f5f984edebe807eea952bd
Andrei Luca <andrei.luca0@gmail.com>
2019-12-16 12:09:43 +0200

    4

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@
 
     $('ul.form li a').click(
         function (e) {
-            console.log("click", e)
+            console.log("click", e, $(this).attr('href'));
             var href=$(this).attr('href');
             console.log("TCL: href", href)
             e.preventDefault(); // prevent the default action
             e.stopPropagation; // stop the click from bubbling
             $(this).closest('ul').find('.selected').removeClass('selected');
             $(this).parent().addClass('selected');
+            console.log("haidi uaai")
             if(href=="game-setup") {
                 console.log("load game setup")
                 $("#placeholder").load("game-setup.html");
